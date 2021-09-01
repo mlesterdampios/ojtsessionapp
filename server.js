a664dd70da0d40384828f17a0b90611acd02898f
Mark Lester Dampios <42646513+mlesterdampios@users.noreply.github.com>
2021-09-02 06:45:51 +0800

    try fix

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+const express = require("express");
+const path = require("path");
 
-const express = require('express');
 const app = express();
 
-app.use(requireHTTPS);
-app.use(express.static('./www'));
+app.use(express.static(__dirname + "/www"));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'www/'}),
-);
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "www", "index.html"));
+});
 
 app.listen(process.env.PORT || 8080);
